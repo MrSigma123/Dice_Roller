da5166d93f16dea2ade80241d3fa4b362fe8cb06
MrSigma123 <karol.czolpinski@gmail.com>
2024-06-08 23:40:22 +0200

    Add project design overview

---
+++ dice_roller.c
@@ -0,0 +1,12 @@
+/* dice_roller.c */
+/* Project overview
+  design functions and divide program by modules
+  implement rolls for different dice types:
+  - tetrahedron
+  - cube
+  - octahedron
+  - pentagonal trapezohedron
+  - dodecahedron
+  - icosahedron
+  add sum, product of different dice combinations
+  */
