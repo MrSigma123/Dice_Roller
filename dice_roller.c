757b7b37ff3f819e5976b523b56bd2658dc37612
MrSigma123 <karol.czolpinski@gmail.com>
2024-11-27 23:59:08 +0100

    Add dice_roller.c main file

--- dice_roller.c
+++ dice_roller.c
@@ -13,3 +13,43 @@
   Divide code with main function file and defined functions
   in separate file and header
   */
+
+#include <stdio.h>
+#include "dice_roller.h"
+
+int main() {
+    int choice, rolls, i;
+    printf("Welcome to Dice Roller!\n");
+    printf("Choose the type of dice to roll:\n");
+    printf("1. Tetrahedron (4 sides)\n");
+    printf("2. Cube (6 sides)\n");
+    printf("3. Octahedron (8 sides)\n");
+    printf("4. Pentagonal Trapezohedron (10 sides)\n");
+    printf("5. Dodecahedron (12 sides)\n");
+    printf("6. Icosahedron (20 sides)\n");
+    scanf("%d", &choice);
+
+    int sides = get_dice_sides(choice);
+    if (sides == -1) {
+        printf("Invalid choice. Exiting.\n");
+        return 1;
+    }
+
+    printf("How many rolls?\n");
+    scanf("%d", &rolls);
+
+    int results[rolls];
+    int sum = 0, product = 1;
+    printf("Rolling %d-sided dice %d times:\n", sides, rolls);
+    for (i = 0; i < rolls; i++) {
+        results[i] = roll_dice(sides);
+        printf("Roll %d: %d\n", i + 1, results[i]);
+        sum += results[i];
+        product *= results[i];
+    }
+
+    printf("Sum of rolls: %d\n", sum);
+    printf("Product of rolls: %d\n", product);
+    return 0;
+}
+
